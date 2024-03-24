efc73e3251c13221e548181a9d54bcba6e4299c6
sailalith <lalithvoop@hotmail.com>
2024-03-24 15:37:36 +0530

    added backend and frontend

--- main.tf
+++ main.tf
@@ -17,3 +17,22 @@ module "frontend" {
   env           = var.env
   zone_id       = var.zone_id
 }
+
+
+module "backend" {
+  depends_on = [module.mysql]
+
+  source        = "./modules/app"
+  instance_type = var.instance_type
+  component     = "backend"
+  env           = var.env
+  zone_id       = var.zone_id
+}
+
+module "mysql" {
+  source        = "./modules/app"
+  instance_type = var.instance_type
+  component     = "mysql"
+  env           = var.env
+  zone_id       = var.zone_id
+}
